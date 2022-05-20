2c09066cfb36656a821cea03ddee5b36500fe85f
Brackhak <chatdanai2543@hotmail.com>
2022-05-20 21:03:43 +0700

    Req popup

--- Web_Appication/client/src/components/pages/RoomRequest/ReqTableRow.js
+++ Web_Appication/client/src/components/pages/RoomRequest/ReqTableRow.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import Popup from './Popup';
 
 export default class ReqTableRow extends Component {
     constructor(props) {
         super(props);
-    
+
         this.state = {
-          Name_Room : "",
-          Name_Building : "",
-          Name_Contributer : "",
+            Name_Room: "",
+            Name_Building: "",
+            Name_Contributer: "",
+            Status_Approve: false,
         };
-      }
+    }
 
     componentDidMount() {
         axios.get("http://localhost:3001/readroom")
@@ -21,9 +23,9 @@ export default class ReqTableRow extends Component {
                     console.log(s1 + " and " + s2)
                     if (s1 === s2) {
                         this.setState({
-                            Name_Room : res.data[i].Name,
-                            Name_Building : res.data[i].Building,
-                            Name_Contributer : res.data[i].Contributer,
+                            Name_Room: res.data[i].Name,
+                            Name_Building: res.data[i].Building,
+                            Name_Contributer: res.data[i].Contributer,
                         })
                         break
                     }
@@ -43,7 +45,17 @@ export default class ReqTableRow extends Component {
                 <td>{this.props.obj.Date_Reserve}</td>
                 <td>{this.props.obj.Name_User}</td>
                 <td>{this.state.Name_Contributer}</td>
-
+                <td>
+                    <input
+                        type="button"
+                        value="Approve"
+                        onClick={() => this.setState({ Status_Approve: !this.state.Status_Approve })}
+                    />
+                    {this.state.Status_Approve && <Popup 
+                    content={<div>จะ Approve ไหม</div>}
+                    handleClose={() => this.setState({Status_Approve : !this.state.Status_Approve})}
+                    />}
+                </td>
             </tr>
         )
     }
